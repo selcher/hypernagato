202443d08880b67f57f2cb38a0be2f137ad5e930
Selcher <selcher123@gmail.com>
2016-12-20 17:29:40 +0800

    Add files for theme

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # hypernagato
-Theme for Hyperterminal
+
+A Simple Theme for [hyper](https://hyper.is/)
+
+### USAGE
+
+Add `hypernagato` to `plugins` in `~/.hyper.js`.
+
+### DISCLAIMER
+
+I claim no ownership to the [image](http://konachan.com/post/show/37081/dark-nagato_yuki-sky-suzumiya_haruhi_no_yuutsu) stored in img/
---
+++ index.js
@@ -0,0 +1,16 @@
+exports.decorteConfig = (config) => {
+    return Object.assign({}, config, {
+        css: `
+            ${config.css} || ''}
+            .terms_terms {
+                background: url('./img/bg.jpg') center;
+                background-size: cover;
+            }
+        `,
+        termCSS: `
+            x-screen {
+                background: transparent !important;
+            }
+        `,
+    });
+};
